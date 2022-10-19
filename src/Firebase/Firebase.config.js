89bde8021bbce0f21108e73cd122fdd3d6f6a85a
hasibferdous <hasibferdous.ict@gmail.com>
2022-10-19 22:36:53 +0600

    context react

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKBpeD0STRK-CvHjn0-sxA3IuNUdAp0QE",
+  authDomain: "auth-route-context-f2436.firebaseapp.com",
+  projectId: "auth-route-context-f2436",
+  storageBucket: "auth-route-context-f2436.appspot.com",
+  messagingSenderId: "422386948714",
+  appId: "1:422386948714:web:9e073a4ab9d07031c30813"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
